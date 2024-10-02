422ce21301fb087d72c918a3ada713be5fac788b
AnnaKarolineNunes <annamiranda894@gmail.com>
2024-10-02 17:18:01 -0300

    teste spring security

--- src/main/java/com/nunes/financeFlow/FinanceFlowApplication.java
+++ src/main/java/com/nunes/financeFlow/FinanceFlowApplication.java
@@ -15,10 +15,12 @@ public class FinanceFlowApplication {
 		String dbUrl = dotenv.get("DATABASE_URL");
 		String dbUser = dotenv.get("DATABASE_USER");
 		String dbPassword = dotenv.get("DATABASE_PASSWORD");
+		String jwtsecret = dotenv.get("JWT_SECRET");
 
 		System.setProperty("spring.datasource.url", dbUrl);
 		System.setProperty("spring.datasource.username", dbUser);
 		System.setProperty("spring.datasource.password", dbPassword);
+		System.setProperty("jwt.secret", jwtsecret);  // Configura a secret key do JWT
 
 		SpringApplication.run(FinanceFlowApplication.class, args);
 	}
