ba9f2e1b192ad3feaa651bc4e898537cc7a26f21
AnnaKarolineNunes <annamiranda894@gmail.com>
2024-10-02 17:03:18 -0300

    correções na segurança das variaveis de ambiente/ arquivo .env

--- src/main/java/com/nunes/financeFlow/FinanceFlowApplication.java
+++ src/main/java/com/nunes/financeFlow/FinanceFlowApplication.java
@@ -1,5 +1,6 @@
 package com.nunes.financeFlow;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FinanceFlowApplication {
 
 	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.configure().load();  // Carrega o arquivo .env
+
+		//acessar as variáveis do .env
+		String dbUrl = dotenv.get("DATABASE_URL");
+		String dbUser = dotenv.get("DATABASE_USER");
+		String dbPassword = dotenv.get("DATABASE_PASSWORD");
+
+		System.setProperty("spring.datasource.url", dbUrl);
+		System.setProperty("spring.datasource.username", dbUser);
+		System.setProperty("spring.datasource.password", dbPassword);
+
 		SpringApplication.run(FinanceFlowApplication.class, args);
 	}
 
