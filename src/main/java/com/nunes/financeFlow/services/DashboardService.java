f8572e5223de663e3f83044469ba01309242c459
AnnaKarolineNunes <annamiranda894@gmail.com>
2024-10-29 20:20:47 -0300

    rotas do dashboard funcionando

--- src/main/java/com/nunes/financeFlow/services/DashboardService.java
+++ src/main/java/com/nunes/financeFlow/services/DashboardService.java
@@ -1,6 +1,5 @@
 package com.nunes.financeFlow.services;
 
-
 import com.nunes.financeFlow.repositories.DespesaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -18,19 +17,33 @@ public class DashboardService {
 
     public ApiResponse<Double> getTotalReceitas(Long userId) {
         Double total = receitaRepository.sumByUsuarioId(userId);
+        if (total == null) {
+            total = 0.0; // Tratando valor nulo como zero
+        }
         return new ApiResponse<>(200, "Total de receitas", total);
     }
 
     public ApiResponse<Double> getTotalDespesas(Long userId) {
         Double total = despesaRepository.sumByUsuarioId(userId);
+        if (total == null) {
+            total = 0.0; // Tratando valor nulo como zero
+        }
         return new ApiResponse<>(200, "Total de despesas", total);
     }
 
     public ApiResponse<Double> getSaldo(Long userId) {
         Double totalReceitas = receitaRepository.sumByUsuarioId(userId);
         Double totalDespesas = despesaRepository.sumByUsuarioId(userId);
+
+        // Tratando valores nulos como zero
+        if (totalReceitas == null) {
+            totalReceitas = 0.0;
+        }
+        if (totalDespesas == null) {
+            totalDespesas = 0.0;
+        }
+
         Double saldo = totalReceitas - totalDespesas;
         return new ApiResponse<>(200, "Saldo atual", saldo);
     }
 }
-
